b8b0c9dec1b43399118d88c2a0bafa5834bd0a33
FedeBirge <fedeikky@gmail.com>
2022-11-09 20:35:53 -0300

    Actualizo ejercicios

--- src/intro/ejercicio13extra.java
+++ src/intro/ejercicio13extra.java
@@ -5,6 +5,8 @@
  */
 package intro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author feder
@@ -15,7 +17,23 @@ public class ejercicio13extra {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Crear un programa que dibuje una escalera de números, donde cada línea de números 
+    //comience en uno y termine en el número de la línea. Solicitar la altura de la escalera al 
+    //usuario al comenzar. Ejemplo: si se ingresa el número 3: 
+    //1
+    //12
+    //123
+        Scanner leer = new Scanner(System.in);
+     int n;
+        System.out.println("Ingrese la altura de la escalera: ");
+        n=leer.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+            
+        }
     }
     
 }
